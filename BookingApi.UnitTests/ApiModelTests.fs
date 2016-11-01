04d387b7ec4dad25bc1a74be667e8e75f80f2fef
Brett Morin <brettmorin2@gmail.com>
2016-11-01 12:39:27 -0700

    Made changes to add Validation Testing

--- BookingApi.UnitTests/ApiModelTests.fs
+++ BookingApi.UnitTests/ApiModelTests.fs
@@ -1,9 +1,45 @@
 ﻿namespace Ploeh.Samples.BookingApi.UnitTests
 
+open System
 open Swensen.Unquote
 open Xunit
 open Ploeh.Samples.Rop
 open Ploeh.Samples.BookingApi
+
+module ValidateTests = 
+  [<Fact>]
+  let ``Validate.reservationValid returns correct result on valid date`` ()=
+    let rendition : ReservationRendition = {
+      Date = "2015-04-15+2:00"
+      Name = "Brett Morin"
+      Email = "bmorin@a.com"
+      Quantity = 4 }
+
+    let actual = Validate.reservationValid rendition
+
+    let expected = Success {
+      Date = 
+        DateTimeOffset(
+          DateTime(2015, 4, 15), 
+          TimeSpan.FromHours 2.)
+      Name = "Brett Morin"
+      Email = "bmorin@a.com"
+      Quantity = 4 }
+    expected =? actual
+
+  [<Fact>]
+  let ``Validate.reservationValid returns correct result on invalid date`` ()=
+    let rendition : ReservationRendition = {
+      Date = "Not a valid date"
+      Name = "Brett Morin"
+      Email = "bmorin@a.com"
+      Quantity = 4 }
+
+    let actual = Validate.reservationValid rendition
+
+    let expected : Result<Reservation, Error> =
+      Failure(ValidationError("Invalid date."))
+    expected =? actual
 
 module ControllerTests =
 
--- BookingApi/ApiModel.fs
+++ BookingApi/ApiModel.fs
@@ -1,6 +1,8 @@
 ﻿namespace Ploeh.Samples.BookingApi
 
 open System.Web.Http
+open System
+open Ploeh.Samples.Rop
 
 [<CLIMutable>]
 type ReservationRendition = {
@@ -8,6 +10,27 @@ type ReservationRendition = {
   Name : string
   Email : string
   Quantity : int}
+
+type Error =
+| ValidationError of string
+
+type Reservation = {
+  Date: DateTimeOffset
+  Name : string
+  Email : string
+  Quantity : int}
+
+module Validate = 
+  let reservationValid (rendition : ReservationRendition) =
+    match rendition.Date |> DateTimeOffset.TryParse with 
+    |  true, date ->
+    Success {
+        Date = date
+        Name = rendition.Name
+        Email = rendition.Email
+        Quantity = rendition.Quantity }
+    | _ -> Failure(ValidationError "Invalid date.")
+
 
 type ReservationsController(imp) =
   inherit ApiController()
